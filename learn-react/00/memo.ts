9e5162e2fac202aa1ca610f97a108caf7e34e304
elecharu <elec_haru@naver.com>
2025-09-15 09:09:20 +0900

    0912 study

---
+++ learn-react/00/memo.ts
@@ -0,0 +1,24 @@
+
+
+// npm install 패키지명--save
+// npm install--save 패키지명
+
+// npm install 패키지명 - D
+// npm install - D 패키지명
+
+// npm install 패키지명--save - dev
+// npm install--save - dev 패키지명
+
+// // public
+// 정적(static) 파일을 보관하는 폴더
+// 빌드 과정에서 번들러의 타겟이 되지 않는 폴더
+// 빌드 과정에서 가공되지 않고 그대로 제공되는 폴더
+
+// src
+// 어플리케이션이 동작하는데 필요한 소스
+
+// 버전 관리
+// 19.2.8
+// 19는 메이저 버전을 의미(Major 버전)
+// 2는 주요 기능 추가를 할 떄 마다 올라가는 Minor 버전을 의미
+// 8은 버그를 수정한 횟수?를 의미
---
+++ learn-react/01/2/src/App.js
@@ -0,0 +1,29 @@
+// Javascript 아님 
+// JSX 형식으로 (Javascript + XML) 확장 문법을 의미
+// 페이스북 (Meta)가 개발
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
